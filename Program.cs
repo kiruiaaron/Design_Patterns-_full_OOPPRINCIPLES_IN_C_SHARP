60c0aeb2f649c263611b6b7520cd9cbfeea0f521
kiruiaaron <kiruiaaron471@gmail.com>
2024-10-16 08:16:51 +0300

    additional changes

--- Program.cs
+++ Program.cs
@@ -72,3 +72,69 @@ foreach (var vehicle in vehicles)
 
 var order = new Order();
 order.PlaceOrder();
+
+
+//Additional practice
+
+var p1 = new Person("Scott","Perez", new DateOnly(1970,1,1));
+var p2 = new Person("Aron","Kirui", new DateOnly(1980,1,2));
+ 
+p1.Pets.Add(new Dog("Fred"));
+p1.Pets.Add(new Dog("Jeff"));
+
+p2.Pets.Add(new Dog("james"));
+p2.Pets.Add(new Cat("Ann"));
+
+List<Person> people = [p1,p2];
+
+
+foreach (var person in people){
+    Console.WriteLine($"{person}");
+    foreach(var pet in person.Pets)
+    {
+        Console.WriteLine($"   {pet}");
+    }
+}
+
+
+
+public class Person(string firstName, string lastName, DateOnly birthday)
+{
+    public string First { get; } = firstName;
+    public string Last { get; } = lastName;
+    public DateOnly Birthday { get; }= birthday;
+    public List<Pet> Pets{get;} = new();
+
+    public override string ToString()
+    {
+        return $"Human {First} {Last}";
+    }
+}
+
+
+public abstract class Pet(string firstName)
+{
+  public string First { get; } = firstName;
+  public abstract string MakeNoise();
+
+    public override string ToString()
+    {
+        return $"{First} and I am a {GetType().Name} and I {MakeNoise()}";
+    }
+
+}
+
+public class Cat( string firstName):Pet(firstName)
+{
+    
+    public override string MakeNoise() =>"meow";
+}
+
+
+public class Dog( string firstName):Pet(firstName)
+{
+    
+    public override string MakeNoise() =>"bark";
+}
+
+
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fed9416325b1da30b3b6606eb5a95255e409bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705fc37011da4710ab55ed857745cc80c456fade")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
